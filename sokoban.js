44221128ca4a9b619cb78644c77849db23dd06bd
Sergey Fedorov <d946@mail.ru>
2017-08-12 21:14:49 +0300

    Добавление перемещения Man

--- sokoban.js
+++ sokoban.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function Sokoban(options) {
     this.options = options;
     this.element = this.options.element;
@@ -8,6 +10,7 @@ function Sokoban(options) {
 
 Sokoban.prototype.init = function() {
     this.loadField();
+    this.listen();
 }
 
 Sokoban.assignData = function(id, my_char, my_class) {
@@ -16,7 +19,7 @@ Sokoban.assignData = function(id, my_char, my_class) {
 }
 
 Sokoban.getType = function(my_char) {
-    for (key in Sokoban.stateChar) {
+    for (var key in Sokoban.stateChar) {
         if (Sokoban.stateChar[key] == my_char) {
             return key;
         }
@@ -32,13 +35,17 @@ Sokoban.ITEM_WALL = 5;
 Sokoban.stateChar = {};
 Sokoban.stateClass = {};
 
+Sokoban.MOVE_TOP = 0;
+Sokoban.MOVE_BOTTOM = 1;
+Sokoban.MOVE_LEFT = 2;
+Sokoban.MOVE_RIGHT = 3;
+
 Sokoban.assignData(Sokoban.ITEM_WALL, '#', 'sokoban__item-wall');
 Sokoban.assignData(Sokoban.ITEM_MAN, '@', 'sokoban__item-man');
 Sokoban.assignData(Sokoban.ITEM_TARGET, '.', 'sokoban__item-target');
 Sokoban.assignData(Sokoban.ITEM_BOX, '$', 'sokoban__item-box');
 Sokoban.assignData(Sokoban.ITEM_SOLVED, '+', 'sokoban__item-solved');
 Sokoban.assignData(Sokoban.ITEM_EMPTY, ' ', 'sokoban__item-empty');
-
 
 Sokoban.prototype.loadField = function() {
     this.element.style.width = (this.options.level.Width * this.options.cellSize) + 'px';
@@ -51,8 +58,13 @@ Sokoban.prototype.loadField = function() {
             } else {
                 this.field[i][j] = Sokoban.getType(' ');
             }
+            if (this.field[i][j] == Sokoban.ITEM_MAN) {
+                this.man.top = i;
+                this.man.left = j;
+            }
             var sokobanItem = document.createElement('div');
             sokobanItem.className = Sokoban.stateClass[this.field[i][j]];
+            sokobanItem.id = 'item' + i + '_' + j;
             sokobanItem.style.width =
                 sokobanItem.style.height = this.options.cellSize + 'px';
 
@@ -62,6 +74,66 @@ Sokoban.prototype.loadField = function() {
         }
     }
     console.log(this.field);
+}
+
+Sokoban.prototype.changeClass = function(top, left, type) {
+    var itemName = 'item' + top + '_' + left;
+    document.getElementById(itemName).className = Sokoban.stateClass[type];
+};
+
+Sokoban.prototype.updateClass = function(top, left) {
+    var itemName = 'item' + top + '_' + left;
+    document.getElementById(itemName).className = Sokoban.stateClass[this.field[top][left]];
+};
+
+Sokoban.prototype.doStep = function(direction) {
+    var ofs = [];
+    ofs[Sokoban.MOVE_TOP] = { 'dx': -1, 'dy': 0 };
+    ofs[Sokoban.MOVE_BOTTOM] = { 'dx': 1, 'dy': 0 };
+    ofs[Sokoban.MOVE_LEFT] = { 'dx': 0, 'dy': -1 };
+    ofs[Sokoban.MOVE_RIGHT] = { 'dx': 0, 'dy': 1 };
+
+    var newman = JSON.parse(JSON.stringify(this.man));
+    newman.left += ofs[direction].dy;
+    newman.top += ofs[direction].dx;
+    //console.log(this.man, newman, this.field[newman.top][newman.left]);
+
+    var action = [];
+    action[Sokoban.ITEM_MAN] = [];
+    //console.dir(Sokoban.ITEM_MAN, action);
+    action[Sokoban.ITEM_MAN][Sokoban.ITEM_EMPTY] = { 'changeOld': Sokoban.ITEM_EMPTY, 'changeNew': Sokoban.ITEM_MAN };
+
+    var oldState = this.field[this.man.top][this.man.left];
+    var newState = this.field[newman.top][newman.left];
+    console.dir(action[oldState]);
+    if (action[oldState] != undefined) {
+        var doAction = action[oldState][newState];
+        if (doAction != 'undefined') {
+            this.field[this.man.top][this.man.left] = doAction['changeOld'];
+            this.field[newman.top][newman.left] = doAction['changeNew'];
+            this.updateClass(this.man.top, this.man.left);
+            this.updateClass(newman.top, newman.left);
+            console.log(this.man, newman);
+            this.man.left = newman.left;
+            this.man.top = newman.top;
+        }
+    }
+}
+
+Sokoban.prototype.listen = function() {
+    var _this = this,
+        directions = {
+            38: Sokoban.MOVE_TOP,
+            40: Sokoban.MOVE_BOTTOM,
+            37: Sokoban.MOVE_LEFT,
+            39: Sokoban.MOVE_RIGHT
+        }
+    window.addEventListener('keydown', function(e) {
+
+        if (e.keyCode in directions) {
+            _this.doStep(directions[e.keyCode]);
+        }
+    })
 }
 
 new Sokoban({
