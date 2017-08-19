1a43c4e6c07c7e830ad76c895733dfb059c535b5
Sergey Fedorov <d946@mail.ru>
2017-08-19 13:49:58 +0300

    Обновлен загрузка уровней

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
-(()=> {
-	levelId = 3;
-})();	
+(() => {
+    levelId = 3;
+})();
 
-window.loadLevel = function (id) {
-	levelId = id;
-	var xhr = false;
-	if (window.XMLHttpRequest) {
-		xhr = new XMLHttpRequest();
-		if (xhr.overrideMimeType)
-			xhr.overrideMimeType('text/xml');
-		else if (window.ActiveXObject) {
-			try {
-				xhr = new ActiveXObject("Msxml2.XMLHTTP");
-			} catch (e) {
-				try {
-					xhr = new ActiveXObject("Microsoft.XMLHTTP");
-				} catch (e) {}
-			}
-		}
-	}
+window.loadLevel = function(id) {
+    levelId = id;
+    var xhr = false;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+        if (xhr.overrideMimeType)
+            xhr.overrideMimeType('text/xml');
+        else if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {}
+            }
+        }
+    }
 
-	if (!xhr) {
-		alert('Ошибка : невозможно создать запрос');
-	}
+    if (!xhr) {
+        alert('Ошибка : невозможно создать запрос');
+    }
 
-	xhr.onreadystatechange = function () {
-		if ((xhr.readyState != 4) || (xhr.status != 200)) {
-			return;
-		}
-		var items = JSON.parse(xhr.responseText);
-		console.dir(items);						
-		new Sokoban({
-			element: document.getElementById('sokoban'),
-			elementdone: document.getElementById('done'),
-			cellSize: 30,
-			levelId: 0,
-			level: [items]
-		});
-	}
-	xhr.ontimeout = function () {
-		alert('Превышено время ожидания запроса');
-	};
-	xhr.open('GET', 'level' + id + '.json', true);
-	xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if ((xhr.readyState != 4) || (xhr.status != 200)) {
+            return;
+        }
+        var items = JSON.parse(xhr.responseText);
+        console.dir(items);
+        if (items.level[id]!= undefined){
+	        console.dir(window.aSokoban);
+	        if (window.aSokoban == undefined) {
+	            window.aSokoban = new Sokoban({
+	                element: document.getElementById('sokoban'),
+	                elementdone: document.getElementById('done'),
+	                cellSize: 30,
+	                levelId: 0,
+	                level: [items.level[id]]
+	            });
+	        }else{
+	        	window.aSokoban.options.level=[items.level[id]];
+	        	window.aSokoban.loadField();
+	        }
+        }
+    }
+    xhr.ontimeout = function() {
+        alert('Превышено время ожидания запроса');
+    };
+    xhr.open('GET', 'levels.json', true);
+    xhr.send(null);
 };
 
 loadLevel(levelId);
